80145539fed106486d7c929b3966d72984c63eb0
Nycto <James@RoundEights.com>
2015-01-16 20:46:37 -0800

    Add HashSet

---
+++ src/collect/HashSet.hx
@@ -0,0 +1,105 @@
+package collect;
+
+import haxe.ds.IntMap;
+
+/**
+ * A set of objects
+ */
+class HashSet<K> {
+
+    /** The map of objects indexed by their hashcode */
+    private var objs = new IntMap<List<K>>();
+
+    /** Determines the hashcode of an object */
+    private var hash: K -> Int;
+
+    /** Determines whether two objects are equal */
+    private var equal: K -> K -> Bool;
+
+    /**
+     * Constructor
+     * @param hashCode Generates a hash for an object
+     * @param equals Determines whether two objects are equal
+     */
+    public inline function new( hash: K -> Int, equal: K -> K -> Bool ) {
+        this.hash = hash;
+        this.equal = equal;
+    }
+
+    /** Determines whether a List contains a specific item */
+    private inline function listContains( list: List<K>, item: K ): Bool {
+        var result: Bool = false;
+        for ( element in list ) {
+            if ( this.equal(element, item) ) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    /** Adds a value */
+    public inline function add( value: K ): HashSet<K> {
+        var hashCode = this.hash(value);
+        var existing = objs.get(hashCode);
+        if ( existing == null ) {
+            var newList = new List();
+            newList.add(value);
+            objs.set( hashCode, newList );
+        }
+        else if ( !listContains(existing, value) ) {
+            existing.add(value);
+        }
+        return this;
+    }
+
+    /** Determines whether this set contains a value */
+    public inline function contains( value: K ): Bool {
+        var existing = objs.get(this.hash(value));
+        return existing == null ? false : listContains(existing, value);
+    }
+
+    /** Removes a value from this set */
+    public inline function remove( value: K ): Void {
+        var existing = objs.get(this.hash(value));
+        if ( existing != null ) {
+            for ( element in existing ) {
+                if ( this.equal(value, element) ) {
+                    existing.remove(element);
+                    break;
+                }
+            }
+        }
+    }
+
+    /** Generates an iterator */
+    public function iterator(): Iterator<K> {
+        return new FlatIterator<K>( objs.iterator() );
+    }
+
+    /** Converts this set to an array */
+    public function toArray(): Array<K> {
+        var result: Array<K> = [];
+        for ( list in objs ) {
+            for ( value in list ) {
+                result.push(value);
+            }
+        }
+        return result;
+    }
+
+    /** Converts this value to a String */
+    public function toString(): String {
+        return "Set(" + toArray().join(", ") + ")";
+    }
+
+    /** Adds all the values from another set into this one */
+    public inline function addAll( other: HashSet<K> ): Void {
+        for ( list in other.objs ) {
+            for ( value in list ) {
+                add(value);
+            }
+        }
+    }
+}
+
---
+++ test/HashSetTest.hx
@@ -0,0 +1,143 @@
+package;
+
+import collect.HashSet;
+import massive.munit.Assert;
+
+private typedef Id = { id: Int };
+private typedef IdValue = { id: Int, value: Int };
+
+class HashSetTest {
+
+    private static function hash( value: Id ) {
+        return value.id;
+    }
+
+    private static function idEqual( a: Id, b: Id ) {
+        return a.id == b.id;
+    }
+
+    private static function equal( a: IdValue, b: IdValue ) {
+        return a.value == b.value;
+    }
+
+    private static function compare( a: IdValue, b: IdValue ) {
+        return a.value == b.value ? 0 : (a.value < b.value ? -1 : 1);
+    }
+
+    @Test public function testBasicFunctionality():Void {
+        var hashset = new HashSet<Id>( hash, idEqual );
+
+        Assert.isFalse( hashset.contains({ id: 50 }) );
+        hashset.add({ id: 50 });
+        Assert.isTrue( hashset.contains({ id: 50 }) );
+
+        hashset.add({ id: 50 });
+        Assert.isTrue( hashset.contains({ id: 50 }) );
+
+        Assert.isFalse( hashset.contains({ id: 90 }) );
+        hashset.add({ id: 90 });
+        Assert.isTrue( hashset.contains({ id: 90 }) );
+    }
+
+    @Test public function testHashCodeConflicts():Void {
+        var hashset = new HashSet<IdValue>( hash, equal );
+
+        Assert.isFalse( hashset.contains({ id: 20, value: 50 }) );
+        Assert.isFalse( hashset.contains({ id: 20, value: 90 }) );
+
+        hashset.add({ id: 20, value: 50 });
+        Assert.isTrue( hashset.contains({ id: 20, value: 50 }) );
+        Assert.isFalse( hashset.contains({ id: 20, value: 90 }) );
+
+        hashset.add({ id: 20, value: 90 });
+        Assert.isTrue( hashset.contains({ id: 20, value: 50 }) );
+        Assert.isTrue( hashset.contains({ id: 20, value: 90 }) );
+    }
+
+    @Test public function testRemove():Void {
+        var hashset = new HashSet<IdValue>( hash, equal );
+
+        hashset.add({ id: 20, value: 50 });
+        hashset.add({ id: 20, value: 90 });
+        hashset.add({ id: 40, value: 130 });
+
+        Assert.isTrue( hashset.contains({ id: 20, value: 50 }) );
+        Assert.isTrue( hashset.contains({ id: 20, value: 90 }) );
+        Assert.isTrue( hashset.contains({ id: 40, value: 130 }) );
+
+        hashset.remove({ id: 20, value: 50 });
+        Assert.isFalse( hashset.contains({ id: 20, value: 50 }) );
+        Assert.isTrue( hashset.contains({ id: 20, value: 90 }) );
+        Assert.isTrue( hashset.contains({ id: 40, value: 130 }) );
+
+        hashset.remove({ id: 40, value: 130 });
+        Assert.isFalse( hashset.contains({ id: 20, value: 50 }) );
+        Assert.isTrue( hashset.contains({ id: 20, value: 90 }) );
+        Assert.isFalse( hashset.contains({ id: 40, value: 130 }) );
+    }
+
+    @Test public function testIteration():Void {
+        var hashset = new HashSet<IdValue>( hash, equal );
+
+        hashset.add({ id: 20, value: 50 });
+        hashset.add({ id: 20, value: 90 });
+        hashset.add({ id: 40, value: 130 });
+        hashset.add({ id: 20, value: 50 });
+
+        var result: Array<IdValue> = [];
+        for ( value in hashset ) {
+            result.push( value );
+        }
+
+        Helper.unsortedArrayEqualsUsing(
+            [
+                { id: 20, value: 50 },
+                { id: 20, value: 90 },
+                { id: 40, value: 130 }
+            ],
+            result,
+            compare
+        );
+    }
+
+    @Test public function testEmptyIteration():Void {
+        var hashset = new HashSet<IdValue>( hash, equal );
+
+        var result = [];
+        for ( value in hashset ) {
+            result.push( value );
+        }
+
+        Helper.arrayEqualsUsing( [], result, equal );
+    }
+
+    @Test public function testAddAll():Void {
+        var hashset = new HashSet<IdValue>( hash, equal );
+        hashset.addAll(
+            new HashSet<IdValue>( hash, equal )
+                .add( { id: 20, value: 90 } )
+                .add( { id: 40, value: 130 } )
+        );
+
+        Helper.unsortedArrayEqualsUsing(
+            [ { id: 20, value: 90 }, { id: 40, value: 130 } ],
+            hashset.toArray(), compare
+        );
+
+        hashset.addAll(
+            new HashSet<IdValue>( hash, equal )
+                .add( { id: 40, value: 130 } )
+                .add( { id: 80, value: 200 } )
+        );
+
+        Helper.unsortedArrayEqualsUsing(
+            [
+                { id: 20, value: 90 },
+                { id: 40, value: 130 },
+                { id: 80, value: 200 }
+            ],
+            hashset.toArray(), compare
+        );
+    }
+}
+
