e8c35095879a34dcaa15f647082f52ae38fecbeb
Nycto <James@RoundEights.com>
2015-01-16 19:19:01 -0800

    Add HashMap

---
+++ src/collect/HashMap.hx
@@ -0,0 +1,126 @@
+package collect;
+
+import haxe.ds.IntMap;
+
+/** A Key/Value tuple */
+private class KeyValue<K, V> {
+    public var key(default, null): K;
+    public var value(default, null): V;
+    public inline function new ( key: K, value: V ) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+/**
+ * Maps a key to a value using custom hash code and equality functions
+ */
+class HashMap<K, V> {
+
+    /** Determines the hashcode of an object */
+    private var hash: K -> Int;
+
+    /** Determines whether two objects are equal */
+    private var equal: K -> K -> Bool;
+
+    /** The keys and values indexed by their hash map */
+    private var objs = new IntMap<Array<KeyValue<K, V>>>();
+
+    /**
+     * Constructor
+     * @param hashCode Generates a hash for an object
+     * @param equals Determines whether two objects are equal
+     */
+    public inline function new( hash: K -> Int, equal: K -> K -> Bool ) {
+        this.hash = hash;
+        this.equal = equal;
+    }
+
+    /** Determines whether a List contains a specific item */
+    private inline function indexOf(
+        list: Array<KeyValue<K, V>>, key: K
+    ): Null<Int> {
+        var result: Null<Int> = null;
+        for ( index in 0...list.length ) {
+            if ( this.equal(list[index].key, key) ) {
+                result = index;
+                break;
+            }
+        }
+        return result;
+    }
+
+    /** Adds a value */
+    public inline function set( key: K, value: V ): Void {
+        var hashCode: Int = this.hash(key);
+
+        var existing: Null<Array<KeyValue<K, V>>> = objs.get(hashCode);
+
+        var representation = new KeyValue(key, value);
+
+        if ( existing == null ) {
+            objs.set( hashCode, [representation] );
+        }
+        else {
+            var index: Null<Int> = indexOf( existing, key );
+            if ( index == null ) {
+                existing.push(representation);
+            }
+            else {
+                existing[index] = representation;
+            }
+        }
+    }
+
+    /** Returns a value */
+    public inline function get( key: K ): Null<V> {
+        var hashCode: Int = this.hash(key);
+
+        var existing: Null<Array<KeyValue<K, V>>> = objs.get(hashCode);
+
+        if ( existing == null ) {
+            return null;
+        }
+        else {
+            var index: Null<Int> = indexOf( existing, key );
+            return index == null ? null : existing[index].value;
+        }
+    }
+
+    /** Removes a value from this hashmap */
+    public inline function unset( key: K ): Void {
+        var hashCode: Int = this.hash(key);
+
+        var existing: Null<Array<KeyValue<K, V>>> = objs.get(hashCode);
+
+        if ( existing != null ) {
+            var index: Null<Int> = indexOf( existing, key );
+            if ( index != null ) {
+                existing.splice(index, 1);
+            }
+        }
+    }
+
+    /** Returns an iterator over the keys in this hashmap */
+    public inline function keys(): Iterator<K> {
+        var iterator = new FlatIterator( objs.iterator() );
+        return {
+            hasNext: function (): Bool {
+                return iterator.hasNext();
+            },
+            next: function (): K {
+                return iterator.next().key;
+            }
+        };
+    }
+
+    /** Returns a string representation of this hashmap */
+    public function toString(): String {
+        var result = new Array<String>();
+        for ( pair in new FlatIterator( objs.iterator() ) ) {
+            result.push( pair.key + " -> " + pair.value );
+        }
+        return "Map(" + result.join(", ") + ")";
+    }
+}
+
---
+++ test/HashMapTest.hx
@@ -0,0 +1,104 @@
+package;
+
+import collect.HashMap;
+import massive.munit.Assert;
+
+class HashMapTest {
+
+    private static function hash( value: { id: Int } ) {
+        return value.id;
+    }
+
+    private static function idEqual( a: {id: Int}, b: {id: Int} ) {
+        return a.id == b.id;
+    }
+
+    private static function equal( a: {value: Int}, b: {value: Int} ) {
+        return a.value == b.value;
+    }
+
+    private static function compare( a: {id: Int}, b: {id: Int} ) {
+        return a.id == b.id ? 0 : (a.id < b.id ? -1 : 1);
+    }
+
+    @Test public function testBasicFunctionality():Void {
+        var hashmap = new HashMap<{ id: Int }, String>( hash, idEqual );
+
+        Assert.isNull( hashmap.get({ id: 50 }) );
+
+        hashmap.set({ id: 50 }, "Fifty");
+        Assert.areEqual( "Fifty", hashmap.get({ id: 50 }) );
+
+        hashmap.set({ id: 50 }, "Fifty is 50");
+        Assert.areEqual( "Fifty is 50", hashmap.get({ id: 50 }) );
+
+        hashmap.set({ id: 60 }, "Sixty");
+        Assert.areEqual( "Sixty", hashmap.get({ id: 60 }) );
+        Assert.areEqual( "Fifty is 50", hashmap.get({ id: 50 }) );
+
+        Assert.isNull( hashmap.get({ id: 90 }) );
+    }
+
+    @Test public function testHashCodeConflicts():Void {
+        var hashmap = new HashMap<{ id: Int, value: Int }, String>(hash, equal);
+
+        Assert.isNull( hashmap.get({ id: 20, value: 50 }) );
+        Assert.isNull( hashmap.get({ id: 20, value: 90 }) );
+
+        hashmap.set({ id: 20, value: 50 }, "Fifty");
+        Assert.areEqual( "Fifty", hashmap.get({ id: 20, value: 50 }) );
+        Assert.isNull( hashmap.get({ id: 20, value: 90 }) );
+
+        hashmap.set({ id: 20, value: 90 }, "Ninety");
+        Assert.areEqual( "Fifty", hashmap.get({ id: 20, value: 50 }) );
+        Assert.areEqual( "Ninety", hashmap.get({ id: 20, value: 90 }) );
+    }
+
+    @Test public function testRemove():Void {
+        var hashmap = new HashMap<{ id: Int, value: Int }, String>(hash, equal);
+
+        hashmap.set({ id: 20, value: 50 }, "Fifty");
+        hashmap.set({ id: 20, value: 90 }, "Ninety");
+        hashmap.set({ id: 440, value: 10 }, "Ten");
+
+        Assert.areEqual("Fifty", hashmap.get({ id: 20, value: 50 }));
+        Assert.areEqual("Ninety", hashmap.get({ id: 20, value: 90 }));
+        Assert.areEqual("Ten", hashmap.get({ id: 440, value: 10 }));
+
+        hashmap.unset({ id: 20, value: 50 });
+        Assert.isNull(hashmap.get({ id: 20, value: 50 }));
+        Assert.areEqual("Ninety", hashmap.get({ id: 20, value: 90 }));
+        Assert.areEqual("Ten", hashmap.get({ id: 440, value: 10 }));
+
+        hashmap.unset({ id: 440, value: 10 });
+        Assert.isNull(hashmap.get({ id: 20, value: 50 }));
+        Assert.areEqual("Ninety", hashmap.get({ id: 20, value: 90 }));
+        Assert.isNull(hashmap.get({ id: 440, value: 10 }));
+
+        hashmap.unset({ id: 20, value: 90 });
+        Assert.isNull(hashmap.get({ id: 20, value: 50 }));
+        Assert.isNull(hashmap.get({ id: 20, value: 90 }));
+        Assert.isNull(hashmap.get({ id: 440, value: 10 }));
+    }
+
+    @Test public function testKeyIteration():Void {
+        var hashmap = new HashMap<{ id: Int }, String>( hash, idEqual );
+
+        Helper.arrayEqualsUsing(
+            [],
+            Helper.toArray( hashmap.keys() ),
+            idEqual
+        );
+
+        hashmap.set({ id: 50 }, "Fifty");
+        hashmap.set({ id: 60 }, "Sixty");
+        hashmap.set({ id: 90 }, "Ninety");
+
+        Helper.unsortedArrayEqualsUsing(
+            [{ id: 50 }, { id: 60 }, { id: 90 }],
+            Helper.toArray( hashmap.keys() ),
+            compare
+        );
+    }
+}
+
